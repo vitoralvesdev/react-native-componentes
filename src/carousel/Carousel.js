64bf905fcdc59439e9a5846c853f8010966f72b0
vitoralvesdev <vitorhugoalvees@gmail.com>
2017-09-05 11:25:47 -0300

    Novo component Carousel

---
+++ src/carousel/Carousel.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ScrollView
+} from 'react-native';
+
+export default class Carousel extends Component {
+    render() {
+        return(
+            <View>
+                <Text> Carousel </Text>
+            </View>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // Componentes 
+import Carousel from './carousel/Carousel';
 import FiltroModal from './filtromodal/FiltroModal';
 import Paginacao from './paginacao/Paginacao';
 
+
+// Exports
 export {
+    Carousel,
     FiltroModal,
     Paginacao
 };
