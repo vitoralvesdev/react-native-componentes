adcd90b12be0c07aa8c409ea15dd268e0b8bfa71
vitoralvesdev <vitorhugoalvees@gmail.com>
2017-09-14 20:08:56 -0300

    Prop fotos

--- src/App.js
+++ src/App.js
@@ -4,34 +4,44 @@ import {
   Text,
   StyleSheet
 } from 'react-native';
-import Header from './header/Header';
+import GaleriaFotos from './galeriaFotos/GaleriaFotos';
+const FOTOS = [
+    {id: 0, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 1, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 2, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 3, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 4, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 5, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 6, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 7, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 8, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 9, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 10, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 11, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 12, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 13, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 14, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 15, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 16, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 17, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 18, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+    {id: 19, nome: 'Foto Um', url: 'https://i.imgur.com/mcT43dV.jpg'},
+];
 
 export default class App extends Component {
     render() {
         return(
-            <View style={estilos.corpo}>
-                <Header titulo="React Native Componentes" />
-                <Text style={estilos.subTitulo}>
-                    ./src/ * Para todos os componentes
-                </Text>
-                <Text style={estilos.subTitulo}>
-                    ./exemplos/ * Para todos os exemplos
-                </Text>      
+            <View >
+                <GaleriaFotos 
+                    titulo="Album de Terror"
+                    qtdFotos={10}
+                    fotos={FOTOS}
+                />      
             </View>
         )
     }
 }
 
 const estilos = StyleSheet.create({
-    corpo : {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    subTitulo : {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    }
+    
 })
--- src/galeriaFotos/GaleriaFotos.js
+++ src/galeriaFotos/GaleriaFotos.js
@@ -5,9 +5,7 @@ import {
     StyleSheet,
     Dimensions,
     Image,
-    ToutchableOpacity,
-    Modal,
-    Flatlist
+    FlatList
 } from 'react-native';
 const {width} = Dimensions.get('window');
 
@@ -15,9 +13,70 @@ export default class GaleriaFotos extends Component {
     constructor(props) {
         super(props);
     }
+
+    renderQtdFotos(qtdFotos) {
+        if (qtdFotos < 0 || qtdFotos == 0) return 'Nenhum Foto';
+        if (qtdFotos == 1) return qtdFotos + ' Foto';
+        if (qtdFotos > 1) return qtdFotos + ' Fotos';
+    }
+
+    renderHeader() {
+        return(
+            <View style={estilos.header}>
+                <Text style={estilos.headerTexto}>{this.props.titulo}</Text>
+                <Text style={estilos.headerTexto}>{this.renderQtdFotos(this.props.qtdFotos)}</Text>
+            </View>
+        )
+    }
+
+    render() {
+        return(
+            <View>
+                {this.renderHeader()}
+                <View style={estilos.fotos}>
+                    <FlatList 
+                        data={this.props.fotos}
+                        keyExtractor={item=>item.id}
+                        numColumns={3}
+                        renderItem={({item})=>(
+                            <View>
+                                <Image style={estilos.foto} source={{uri: item.url}} />
+                            </View>
+                        )}
+                    />
+                </View>
+            </View>
+        )
+    }
 }
     
+GaleriaFotos.PropTypes = {
+    titulo: PropTypes.string,
+    qtdFotos: PropTypes.number,
+    fotos: PropTypes.object
+}
 
 const estilos = StyleSheet.create({
-  
+    header : {
+        width: width,
+        height: 60,
+        position: 'absolute',
+        top: 0,
+        backgroundColor: 'red'
+    },
+    headerTexto : {
+        color: '#000000',
+        fontSize: 20,
+        marginLeft: 20
+    },
+    fotos : {
+        marginTop: 65
+    },
+    foto : {
+        width: width/3,
+        height: 100,
+        margin: 5,
+        backgroundColor: '#E2E2E2'
+    }
+    
 })
