9edb1530d7af9a979e67f66c43834204db341334
vitoralvesdev <vitorhugoalvees@gmail.com>
2017-10-12 20:36:28 -0300

    Correção de cagada

--- src/App.js
+++ src/App.js
@@ -4,41 +4,19 @@ import {
   Text,
   StyleSheet
 } from 'react-native';
-import GaleriaFotos from './galeriaFotos/GaleriaFotos';
-
-const FOTOS = [
-    {id: 0, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 1, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 2, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 3, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 4, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 5, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 6, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 7, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 8, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 9, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 10, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 11, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 12, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 13, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 14, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 15, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 16, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 17, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 18, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-    {id: 19, nome: 'Pennywise', url: 'https://i.imgur.com/mcT43dV.jpg'},
-];
+import Header from './header/Header';
 
 export default class App extends Component {
     render() {
         return(
             <View style={estilos.corpo}>
-                <GaleriaFotos 
-                    titulo="Album de Terror"
-                    qtdFotos={FOTOS.length}
-                    fotos={FOTOS}
-                    layout="lista"
-                />      
+                <Header titulo="React Native Componentes" />
+                <Text style={estilos.subTitulo}>
+                    ./src/ * Para todos os componentes
+                </Text>
+                <Text style={estilos.subTitulo}>
+                    ./exemplos/ * Para todos os exemplos
+                </Text>      
             </View>
         )
     }
